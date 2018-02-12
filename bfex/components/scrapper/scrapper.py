9da091cb54c8f1ac3b9f47862420892a2122e897
Mandy Meindersma <meinders@ualberta.ca>
2018-02-12 09:29:26 -0700

    After Garry's changes

--- bfex/components/scrapper/scrapper.py
+++ bfex/components/scrapper/scrapper.py
@@ -1,26 +1,36 @@
 from bs4 import BeautifulSoup
-import urllib3
+import requests
+import json
+import re
 
-urllib3.disable_warnings()
-http = urllib3.PoolManager()
+# this should be done with a real service but
+# I do not know how that will work yet
+data = json.load(open(r'../../../../faculty.json'))
 
+base_url = "https://www.ualberta.ca/science/about-us/contact-us/faculty-directory/"
 
-# this is where we would do json parsing to get the names?!?!?!
+for item in data:
+    name_from_json = item['name']
+    name_from_json = re.sub('[.]', '', name_from_json)
+    name_list = re.findall('[A-Z][^A-Z]*', name_from_json)
+    formated_name = "-".join(name_list).lower()
 
-# concatenate names into url? here ?!?!!?
+    # concatenate names into url
+    url = base_url + formated_name
 
-url = "https://www.ualberta.ca/science/about-us/contact-us/faculty-directory/william-allison"
-response = http.request("GET", url)
-soup = BeautifulSoup(response.data, "html.parser")
-links = soup.find_all("a")
-for link in links:
-    try:
-        if 'orcid' in link.attrs['href']:
-            print("ORCID ID")
-            print(link.attrs['href'])
-        if "researcherid" in link.attrs['href']:
-            print("ResearcherID")
-            print(link.attrs['href'])
-    except KeyError:
-        # not all 'a' tags have the links we want
-        pass
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("a")
+    for link in links:
+        try:
+            if 'orcid' in link.attrs['href']:
+                print(formated_name)
+                print("    "+"ORCID ID")
+                print("    "+link.attrs['href'])
+            if "researcherid" in link.attrs['href']:
+                print(formated_name)
+                print("    "+"ResearcherID")
+                print("    "+link.attrs['href'])
+        except KeyError:
+            # not all 'a' tags have the links we want
+            pass
