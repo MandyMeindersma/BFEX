c9d8a6469125c8b7206118a08b7aea3f9801fed1
GarryBullock <gbullock@ualberta.ca>
2018-04-03 23:03:11 -0600

    Allow names to be searched as well as keywords.

--- web/bfex/blueprints/search_api.py
+++ web/bfex/blueprints/search_api.py
@@ -25,17 +25,31 @@ class SearchAPI(Resource):
         try:
             if dept is not None:
                 dept_filter = set([x.strip() for x in dept.split(',')])
+            else:
+                dept_filter = set()
         except:
             abort(400)
 
         q_parser = parser.QueryParser()
         q_builder = builder.QueryBuilder()
 
-        pf_query = q_parser.parse_query(query)
-        elastic_query = q_builder.build(pf_query)
+        try:
+            pf_query = q_parser.parse_query(query)
+        except parser.QueryException:
+            abort(400)
 
-        response = Keywords.search().query(elastic_query).execute()
-
+        keywords_elastic_query = q_builder.build(pf_query)        
+        response = Keywords.search().query(keywords_elastic_query).execute()    
+        faculty_with_keywords = SearchAPI.get_faculty_with_keywords(response)
+        
+        SearchAPI.add_name_search_results(faculty_with_keywords, pf_query)
+        
+        return {
+            "data": SearchAPI.create_results(faculty_with_keywords, dept_filter)
+        }
+    
+    @staticmethod
+    def get_faculty_with_keywords(response):
         # Build keyword set for each faculty
         keyword_schema = KeywordSchema(exclude=['faculty_id'])
         faculty_with_keywords = {}
@@ -43,8 +57,12 @@ class SearchAPI(Resource):
             if keywords.faculty_id not in faculty_with_keywords:
                 faculty_with_keywords[keywords.faculty_id] = []
 
-            faculty_with_keywords[keywords.faculty_id].append(keywords)        
+            faculty_with_keywords[keywords.faculty_id].append(keywords) 
+        
+        return faculty_with_keywords
 
+    @staticmethod
+    def create_results(faculty_with_keywords, dept_filter):
         # Build json representations with nested keywords
         schema = FacultySchema()
         results = []
@@ -52,15 +70,29 @@ class SearchAPI(Resource):
             faculty = Faculty.safe_get(faculty_id)
 
             if faculty is None or \
-                (dept is not None and faculty.department not in dept_filter): 
+                (len(dept_filter) > 0 and faculty.department not in dept_filter): 
                 continue
             
             faculty.generated_keywords = keywords
             results.append(schema.dump(faculty))
-            
-        return {
-            "data": results
-        }
+        
+        return results
 
+    @staticmethod
+    def add_name_search_results(faculty_with_keywords, pf_query):
+        # Add functionality of searching names in query.
+        q_builder = builder.QueryBuilder()
+        name_elastic_query = q_builder.build(pf_query, search_field="full_name")
+        names_response = Faculty.search().query(name_elastic_query).execute()
+        
+        for faculty in names_response:
+            # We already have the faculty who was searched in the results.
+            if faculty.faculty_id in faculty_with_keywords:
+                continue
+
+            faculty_keywords = Keywords.search()\
+                .query('match', faculty_id=faculty.faculty_id).execute()
+            
+            faculty_with_keywords[faculty.faculty_id] = faculty_keywords
 
 api.add_resource(SearchAPI, '/search')
