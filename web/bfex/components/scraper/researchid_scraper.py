e5f6313d68123121fe12a884693095e5a94a170f
Mandy Meindersma <meinders@ualberta.ca>
2018-04-05 10:12:44 -0600

    fixing test

--- web/bfex/components/scraper/researchid_scraper.py
+++ web/bfex/components/scraper/researchid_scraper.py
@@ -18,6 +18,32 @@ class ResearchIdScraper(Scraper):
         found_d = False
         scrapp = Scrapp()
 
+        # Get the keywords and description
+        for section in freebies:
+            if (found is True and found_d is True):
+                break
+            contents = section.find_all("td")
+            i = 0
+            for content in contents:
+                if ("Keywords:" == str(content.string) and not found):
+                    # Get just the keywords and strip out other characters
+                    keywords = contents[i+1].contents[0].strip()
+                    keywords = keywords.replace("\n", "").replace(" ", "")
+                    keywords = keywords.replace("\r", "")
+                    keywords = keywords.replace("\xa0", "").split(";")
+                    scrapp.add_meta("keywords", keywords)
+                    found = True
+                if ("Description:" in str(content.string) and not found_d):
+                    # Get the description and strip out other characters
+                    keywords = contents[i+1].string.replace("\xa0", "")
+                    scrapp.add_meta("description", keywords)
+                    found_d = True
+                i += 1
+        
+        # May append a blank scrape, but first in the list is for freebies
+        scrapp.set_source(self.type)
+        scrapps.append(scrapp)    
+            
         imgs = soup.find_all("img")
         links = []
         for img in imgs:
@@ -44,31 +70,6 @@ class ResearchIdScraper(Scraper):
                 except requests.exceptions.ConnectionError:
                     print("\nThere was a connection error\n")
 
-        # Get the keywords and description
-        for section in freebies:
-            if (found is True and found_d is True):
-                break
-            contents = section.find_all("td")
-            i = 0
-            for content in contents:
-                if ("Keywords:" == str(content.string) and not found):
-                    # Get just the keywords and strip out other characters
-                    keywords = contents[i+1].contents[0].strip()
-                    keywords = keywords.replace("\n", "").replace(" ", "")
-                    keywords = keywords.replace("\r", "")
-                    keywords = keywords.replace("\xa0", "").split(";")
-                    scrapp.add_meta("keywords", keywords)
-                    found = True
-                if ("Description:" in str(content.string) and not found_d):
-                    # Get the description and strip out other characters
-                    keywords = contents[i+1].string.replace("\xa0", "")
-                    scrapp.add_meta("description", keywords)
-                    found_d = True
-                i += 1
-
-        # May append a blank scrape, but first in the list is for freebies
-        scrapp.set_source(self.type)
-        scrapps.append(scrapp)
         titles = soup.find_all("input")
 
         # Get the titles
