4f3d0265c9e9cc3337fec5f110a9ded069d8854b
Lane Scobie <LaneScobie@Lanes-MacBook-Pro.local>
2018-04-05 09:23:41 -0600

    Stores the freebie keywords in the keyword index

--- web/bfex/components/data_pipeline/tasks/researchid.py
+++ web/bfex/components/data_pipeline/tasks/researchid.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from bfex.components.scraper.scraper_factory import ScraperFactory
 from bfex.components.scraper.scraper_type import ScraperType
-from bfex.models import Faculty, Document
+from bfex.models import Faculty, Document, Keywords
 from bfex.common.utils import URLs, FacultyNames
 from bfex.common.exceptions import WorkflowException, ScraperException
 from bfex.components.data_pipeline.tasks.task import Task
@@ -61,6 +61,12 @@ class ResearchIdPageScrape(Task):
             doc = Document()
             doc.faculty_id = faculty.faculty_id
             doc.source = "ResearchId"
+
+            keywords = Keywords()
+            keywords.faculty_id = faculty.faculty_id
+            keywords.datasource = "freebies"
+            keywords.approach_id  = "4"
+
             try:
                 doc.text = keywords_and_description.meta_data["description"]
             except:
@@ -68,11 +74,13 @@ class ResearchIdPageScrape(Task):
                 doc.text = ""
             try:
                 doc.user_keywords = keywords_and_description.meta_data["keywords"]
+                keywords.keywords = keywords_and_description.meta_data["keywords"]
             except:
                 print("No keywords")
             doc.date = datetime.now()
             doc.save()
-
+            keywords.save()
+            
             for scrapp in titles:
                 doc = Document()
                 doc.source = "ResearchId"
